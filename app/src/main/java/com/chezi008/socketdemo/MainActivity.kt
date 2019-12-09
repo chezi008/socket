615a428b03cbbfa769f9db3002981c4a1d3edb87
sailor <552104693@qq.com>
2019-12-09 14:38:15 +0800

    增加不需要验证的socket

--- app/src/main/java/com/chezi008/socketdemo/MainActivity.kt
+++ app/src/main/java/com/chezi008/socketdemo/MainActivity.kt
@@ -2,11 +2,41 @@ package com.chezi008.socketdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.chezi008.android.socket.ISocketListener
+import com.chezi008.android.socket.NormalSocket
+import com.chezi008.android.socket.SSLTcpSocket
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity(), ISocketListener {
+    override fun recv(data: ByteArray?, offset: Int, size: Int) {
+        Log.d("MainActivity", "recv: ")
+    }
 
+    override fun onSuccess() {
+        Log.d("MainActivity", "onSuccess: ")
+    }
+
+    override fun onFaild(t: Throwable?) {
+        Log.d("MainActivity", "onFaild: ")
+    }
+
+    override fun onClose() {
+        Log.d("MainActivity", "onClose: ")
+    }
+
+    companion object{
+        const val IP = ""
+        const val port = 6688
+    }
+
+    private var mTcpClient: NormalSocket? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        conect.setOnClickListener {
+            mTcpClient = NormalSocket(IP, port,this)
+            mTcpClient?.connect(this@MainActivity)
+        }
     }
 }
--- libsocket/src/main/java/com/chezi008/android/socket/NormalSocket.java
+++ libsocket/src/main/java/com/chezi008/android/socket/NormalSocket.java
@@ -3,10 +3,14 @@ package com.chezi008.android.socket;
 import android.content.Context;
 
 import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * @author ：chezi008 on 2018/7/18 15:48
@@ -14,36 +18,37 @@ import javax.net.ssl.TrustManagerFactory;
  * 1、TCP应建立一个心跳机制，不然长时间没有数据的传输会被关闭。
  * @email ：chezi008@163.com
  */
-public class SSLTcpSocket extends BaseTcpSocket {
-
+public class NormalSocket extends BaseTcpSocket {
     private static final String CLIENT_AGREEMENT = "TLS";
-    private static final String CLIENT_TRUST_MANAGER = "X509";
-    private static final String CLIENT_TRUST_KEYSTORE = "BKS";
-
-    public SSLTcpSocket(String ip, int port) {
+    public NormalSocket(String ip, int port) {
         super(ip, port);
     }
 
-    public SSLTcpSocket(String ip, int port, ISocketListener listener) {
+    public NormalSocket(String ip, int port, ISocketListener listener) {
         super(ip, port, listener);
     }
-
-    public void setSSLConfig(int keyPath, String keyPwd) {
-        mKeyPath = keyPath;
-        mKeyPwd = keyPwd;
-    }
-
-    private int mKeyPath;
-    private String mKeyPwd;
 
     @Override
     protected void initSocket(Context ctx) throws Exception {
         SSLContext sslContext = SSLContext.getInstance(CLIENT_AGREEMENT);
-            TrustManagerFactory trustManager = TrustManagerFactory.getInstance(CLIENT_TRUST_MANAGER);
-        KeyStore tks = KeyStore.getInstance(CLIENT_TRUST_KEYSTORE);
-        tks.load(ctx.getResources().openRawResource(mKeyPath), mKeyPwd.toCharArray());
-        trustManager.init(tks);
-        sslContext.init(null, trustManager.getTrustManagers(), null);
+        TrustManager manager = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+        sslContext.init(null, new TrustManager[]{manager}, null);
+
         mSocket = sslContext.getSocketFactory().createSocket(mHost, mPort);
         mSocket.setReceiveBufferSize(RECEIVE_BUFFER_SIEZE);
         mSocket.setSoTimeout(SOCKET_TIME_OUT);
